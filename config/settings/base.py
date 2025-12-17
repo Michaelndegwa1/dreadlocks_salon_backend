088b0689a04c438e6519880a75fd5d227b526cb3
ADMIN <michaelndegwa997@gmail.com>
2025-12-17 12:16:22 +0300

    feat: Implement initial Django base settings, including third-party app integrations and REST Framework configuration.

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
